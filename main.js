47a6b75595cd938aeac15bb282c29c0eb8ecd967
jeesun <luckymeday@gmail.com>
2020-07-08 19:29:11 +0700

    first

---
+++ main.js
@@ -0,0 +1,71 @@
+let guessesRemaining = 5;
+let history = [];
+
+function guessNumber() {
+    guessesRemaining = guessesRemaining - 1;
+
+    if (guessesRemaining <= 0) {
+        document.getElementById("guessesRemaining").innerHTML = 0;
+        document.getElementById("resultArea").innerHTML = `Game Over`;
+        return;
+    } document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+
+    let userNumber = document.getElementById("guessingArea").value;
+    console.log(userNumber, "random:", randomNumber)
+    if (userNumber > randomNumber) {
+        document.getElementById("resultArea").innerHTML = `Too high`
+    } else if (userNumber < randomNumber) {
+        document.getElementById("resultArea").innerHTML = `Too low`
+    } else if (userNumber == randomNumber) {
+        document.getElementById("resultArea").innerHTML = `Correct`
+    }
+    history.push(userNumber);
+    console.log(history);
+    document.getElementById("historyArea").innerHTML = `You've tried ${history}`
+}
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+
+
+
+function reset() {
+    guessesRemaining = 5;
+    document.getElementById('guessesRemaining').innerHTML = guessesRemaining
+    history = []
+    document.getElementById('historyArea').innerHTML = history
+    document.getElementById('resultArea').innerHTML = ''
+    randomNumber = Math.floor(Math.random() * 100 + 1)
+    console.log(randomNumber)
+}
+
+let time = 0 // time start from 0
+let myTime; // timer will be assign to this variable
+function timecounting() {
+    myTime = setInterval(() => {
+        time += 1
+        document.getElementById('timecount').innerHTML = time
+    }, 1000)// every 1 second, it will add 1 into time variable (computer use millisecond so 1000 is 1 second)
+}
+timecounting()// fire the timecounting function!!
+
+
+
+
+// 0. generate the random number (set the range 1~100)
+//     -> Math.floor(Math.random() * 100) => 0~99, so add +1
+// 1. read the number that user type
+// 2. compare the user typed number and ramdom number
+// 3. if user number is greater > than random numner, show the msg too high
+// 4. if user number is smaller < than random numner, show the msg too low
+// 5. if user number is as same === as random numner, show the msg correct
+// --> Q) then how can we generate the random number at the beginning? -> 0.
+// 7. when the user guess, remain chance -1
+// 8. when remained chance is 0, then game over
+
+// 9. when the user click the reset button, game should be reset
+// 9-1. chance go back to 5(beginning chance)
+// 9-2. reset random number
+// 9-3. reset history
+// 9-4. reset input box
+// 9-5. reset result
